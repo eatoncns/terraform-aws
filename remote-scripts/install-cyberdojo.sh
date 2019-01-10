8267ad8f840499cffe690f5cfaa1e60a5e1f4e6b
Philip Meier <philip.meier06@googlemail.com>
2019-01-10 15:30:13 +0000

    Update AMI creation to restrict language options pulled

--- remote-scripts/install-cyberdojo.sh
+++ remote-scripts/install-cyberdojo.sh
@@ -2,4 +2,5 @@
 set -e
 
 chmod +x /tmp/remote-scripts/cyber-dojo
+sudo /tmp/remote-scripts/cyber-dojo start-point create languages --list=/tmp/remote-scripts/start-points-languages/languages_list
 sudo /tmp/remote-scripts/cyber-dojo up
