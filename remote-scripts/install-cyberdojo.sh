71f2d54e5e4b7207e8e425c69b6e4f289cd99376
Philip Meier <philip.meier06@googlemail.com>
2019-01-12 15:34:02 +0000

    Point custom exercises to my repo

--- remote-scripts/install-cyberdojo.sh
+++ remote-scripts/install-cyberdojo.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -e
 
-chmod +x /tmp/remote-scripts/cyber-dojo
-sudo /tmp/remote-scripts/cyber-dojo start-point create languages --list=/tmp/remote-scripts/start-points-languages/languages_list
-sudo /tmp/remote-scripts/cyber-dojo up
+cp /tmp/remote-scripts/cyber-dojo ./
+chmod +x cyber-dojo
+sudo ./cyber-dojo start-point create languages --list=/tmp/remote-scripts/start-points-languages/languages_list
+sudo ./cyber-dojo start-point create custom --git=https://github.com/eatoncns/cpp-training-custom.git
+sudo ./cyber-dojo up
