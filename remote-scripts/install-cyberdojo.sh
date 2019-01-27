ebeb25f3399b6986e595caf67e9c12d07cafb188
Philip Meier <philip.meier06@googlemail.com>
2019-01-27 20:26:48 +0000

    Update repo to point to 8thLight exercises

--- remote-scripts/install-cyberdojo.sh
+++ remote-scripts/install-cyberdojo.sh
@@ -4,5 +4,5 @@ set -e
 cp /tmp/remote-scripts/cyber-dojo ./
 chmod +x cyber-dojo
 sudo ./cyber-dojo start-point create languages --list=/tmp/remote-scripts/start-points-languages/languages_list
-sudo ./cyber-dojo start-point create custom --git=https://github.com/eatoncns/cpp-training-custom.git
+sudo ./cyber-dojo start-point create custom --git=https://github.com/8thlight/cpp-training-custom.git
 sudo ./cyber-dojo up
